a2d7f11459a8c57b5e11eee52350082fba88b120
angalcs <soroush.products@gmail.com>
2021-05-30 22:43:03 +0430

    Initial commit

--- assets/js/email.js
+++ assets/js/email.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#submit').click(function() {
         $('#contactForm').attr('action',
                        'mailto:soroush.products@gmail.com?subject=' +
-                       $('#msg_subject').val() + '&body=' + $('#name').val() + "\n" + $('#message').val());
+                       $('#msg_subject').val() + '&body=' + $('#message').val());
         $('#contactForm').submit();
     });
 });
